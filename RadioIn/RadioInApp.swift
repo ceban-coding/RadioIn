f6f3c3d0f7e0aa2d622f36eca2f75d46381ff225
Ion Ceban <ceban.ion1992@gmail.com>
2021-07-07 09:58:55 -0500

    Initial Commit

---
+++ RadioIn/RadioInApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioInApp.swift
+//  RadioIn
+//
+//  Created by Ion Ceban on 7/7/21.
+//
+
+import SwiftUI
+
+@main
+struct RadioInApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
